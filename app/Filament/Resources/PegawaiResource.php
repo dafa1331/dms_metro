493e3921f610c60f2a444937f2decb024b9ff061
dafa1331 <dafafifa001@gmail.com>
2026-03-05 11:46:59 +0700

    update pegawai

--- app/Filament/Resources/PegawaiResource.php
+++ app/Filament/Resources/PegawaiResource.php
@@ -15,6 +15,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Maatwebsite\Excel\Facades\Excel;
 
 class PegawaiResource extends Resource
 {
